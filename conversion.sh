2341499a8a101c797ec9515502659fbfb5c1ca0f
Thomas Belahi <belahi@ipgp.fr>
2016-04-20 18:38:19 +0200

    modified and commented conversion.sh

--- conversion.sh
+++ conversion.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-pandoc --variable classoption:twocolumn \
-       --variable papersize:a4paper \
+# convert the cheatsheet sheet.md in a nice pdf doc using pandoc
+# options don't always work depending on your version or pandoc
+pandoc --variable classoption=twocolumn \
+       --variable papersize=a4paper \
        -s sheet.md \
        -o sheet.pdf
 
