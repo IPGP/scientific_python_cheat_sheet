10736402cb618f083c27bd63c39012f4c879aac0
Matthias <MMesch@users.noreply.github.com>
2016-06-16 16:56:24 +0200

    little fix

--- print_version/generate_pdf.sh
+++ print_version/generate_pdf.sh
@@ -1,5 +1,3 @@
-convert page1.png page2.png -page a3 \
-        -level 50%,93% -colors 64 high_contrast.pdf
+convert page1.png page2.png -page a3 -level 50%,93% -colors 64 high_contrast.pdf
 
-convert page1.png page2.png -page a3 \
-        -colors 64 low_contrast.pdf
+convert page1.png page2.png -page a3 -colors 64 low_contrast.pdf
