26b418bfff9da99906521d89ff51d8d2adc591b3
Matthias <MMesch@users.noreply.github.com>
2016-06-16 14:27:20 +0200

    smaller pdfs

--- print_version/generate_pdf.sh
+++ print_version/generate_pdf.sh
@@ -1,5 +1,5 @@
-convert page1.png page2.png -page a4 -level 50%,93% -resize 2480x3506\
-                            -units PixelsPerInch -density 300x300 high_contrast.pdf
+convert page1.png page2.png -page a4 -resize 2480x3508 -units PixelsPerInch \
+        -density 300x300 -level 50%,93% -colors 64 -repage a4 high_contrast.pdf
 
-convert page1.png page2.png -page a4 -resize 2480x3506\
-                            -units PixelsPerInch -density 300x300 low_contrast.pdf
+convert page1.png page2.png -page a4 -resize 2480x3508 -units PixelsPerInch \
+        -density 300x300 -colors 64 -repage a4 low_contrast.pdf
