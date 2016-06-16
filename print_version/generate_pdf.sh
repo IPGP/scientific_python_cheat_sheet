c12151c4cfaee509035cd18ad2cb4804b4398f2b
Matthias <MMesch@users.noreply.github.com>
2016-06-16 16:48:23 +0200

    better pdf sizes

--- print_version/generate_pdf.sh
+++ print_version/generate_pdf.sh
@@ -1,5 +1,5 @@
-convert page1.png page2.png -page a4 -resize 2480x3508 -units PixelsPerInch \
-        -density 300x300 -level 50%,93% -colors 64 -repage a4 high_contrast.pdf
+convert page1.png page2.png -page a3 \
+        -level 50%,93% -colors 64 high_contrast.pdf
 
-convert page1.png page2.png -page a4 -resize 2480x3508 -units PixelsPerInch \
-        -density 300x300 -colors 64 -repage a4 low_contrast.pdf
+convert page1.png page2.png -page a3 \
+        -colors 64 low_contrast.pdf
