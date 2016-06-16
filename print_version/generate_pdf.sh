c40f2cd2a6c03de337e23a24a75b55d2ddb1f09f
Matthias <MMesch@users.noreply.github.com>
2016-06-16 14:02:34 +0200

    a4 page pdfs

--- print_version/generate_pdf.sh
+++ print_version/generate_pdf.sh
@@ -1,2 +1,5 @@
-convert page1.png page2.png -level 50%,93% high_contrast.pdf
-convert page1.png page2.png -level 0%,100% low_contrast.pdf
+convert page1.png page2.png -page a4 -level 50%,93% -resize 2480x3506\
+                            -units PixelsPerInch -density 300x300 high_contrast.pdf
+
+convert page1.png page2.png -page a4 -resize 2480x3506\
+                            -units PixelsPerInch -density 300x300 low_contrast.pdf
